1106f1aa170be0575aabac2fbebde22aa3e42e04
mjones <matt.jones@fitchsolutions.com>
2020-02-29 23:03:13 -0600

    removing security from the server

--- server/src/main/java/com/dmrtech/monolith/WebSecurityConfig.java
+++ server/src/main/java/com/dmrtech/monolith/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.dmrtech.monolith;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
@@ -34,6 +37,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring().antMatchers(POST, "/**");
     }
 
     @Bean
