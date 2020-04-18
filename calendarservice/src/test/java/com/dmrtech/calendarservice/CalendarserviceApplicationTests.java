5ce09a6ac3726835090639d4d6c350d52c72cdf5
mjones <matt.jones@fitchsolutions.com>
2020-04-18 12:59:21 -0500

    Initial commit for the calendar service.

---
+++ calendarservice/src/test/java/com/dmrtech/calendarservice/CalendarserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dmrtech.calendarservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // tests for the calendar service.
+    }
+
+}
